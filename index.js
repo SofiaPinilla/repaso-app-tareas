b1b1b05d300919651e90116f2dc534dabdf213e0
SofiaPinilla <contact@sofiapinilla.com>
2022-06-07 11:37:48 +0200

    swagger implemented

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 const { dbConnection } = require("./config/config");
-
+const swaggerUI = require('swagger-ui-express')
+const docs = require('./docs/index')
 app.use(express.json());
 
 dbConnection();
 
 app.use('/tasks',require('./routes/tasks'))
 
+app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs))
 app.listen(port, () => console.log("Servidor levantado en el puerto " + port));
